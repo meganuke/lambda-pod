ab72fd79b8308812f0c3c5fb92373b24026b7ade
meganuke <meganuke@hotmail.com>
2025-08-19 12:47:32 +1000

    added provider to account

---
+++ account/providers.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  alias  = "member"
+  region = var.aws_region
+
+  assume_role {
+    role_arn     = local.account_org_admin_role
+    session_name = "TerraformFoundation"
+  }
+}
