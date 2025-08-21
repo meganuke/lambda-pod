c185d36e8c687432cf430ecef93f9d6526d7cd7c
meganuke <meganuke@hotmail.com>
2025-08-21 14:45:29 +1000

    admin role

--- sso/providers.tf
+++ sso/providers.tf
@@ -1,12 +1,8 @@
 provider "aws" {
   alias  = "member"
   region = var.aws_region
-  dynamic "profile" {
-    for_each = var.aws_profile != null && var.aws_profile != "" ? [var.aws_profile] : []
-    content {
-      profile = profile.value
-    }
-  }
+  profile = var.aws_profile != null && var.aws_profile != "" ? var.aws_profile : null
+   
   assume_role {
     role_arn     = local.main_account_admin_role
   }
